ae7550381d7a9b33651a927f93be55667347d47d
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-09-21 14:03:10 +0500

    creates pages and routes with the help of react router dom

---
+++ BlogApp/Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { About, Dashboard, Home, Projects, SignIn, SignUp } from './pages';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
