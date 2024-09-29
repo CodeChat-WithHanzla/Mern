a16f74963bb54ab87c5e932e9cb00929ff789f3b
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-09-29 16:23:00 +0500

    Dashboard is now private route

--- BlogApp/Frontend/src/App.jsx
+++ BlogApp/Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { About, Dashboard, Home, Projects, SignIn, SignUp } from './pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Header, Footer } from './components';
+import PrivateRoute from './components/PrivateRoute';
 function App() {
   return (
     <BrowserRouter>
@@ -9,7 +10,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
         <Route path='/projects' element={<Projects />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
