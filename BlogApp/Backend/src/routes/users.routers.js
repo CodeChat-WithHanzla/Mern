1411dab00b5c3f5e96dc38128aa84a588dccaca1
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-10-15 11:11:19 +0500

    adding middleware

--- BlogApp/Backend/src/routes/users.routers.js
+++ BlogApp/Backend/src/routes/users.routers.js
@@ -2,5 +2,5 @@ import { Router } from "express";
 import { updateUser } from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const userRouter = Router();
-userRouter.put("/update/:userId", updateUser);
+userRouter.put("/update/:userId", verifyJWT, updateUser);
 export default userRouter;
