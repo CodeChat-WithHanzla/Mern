1563725dba369cb4a898b5228bcf2e83325bbdb6
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-10-15 11:12:18 +0500

    Install cookie parser and adjust the errorhandling middleare after all routes

--- BlogApp/Backend/src/app.js
+++ BlogApp/Backend/src/app.js
@@ -2,11 +2,14 @@ import express from "express";
 import { authRouter, userRouter } from "./routes/index.js";
 import cors from "cors";
 import { errorHandler } from "./utils/errorHandler.js";
+import cookieParser from "cookie-parser";
 const app = express();
+// Dependencies
 app.use(express.json());
 app.use(cors());
-app.use(errorHandler);
+app.use(cookieParser());
+// Routes
 app.use("/api/v1", authRouter);
 app.use("/api/v1", userRouter);
-
+app.use(errorHandler);
 export default app;
