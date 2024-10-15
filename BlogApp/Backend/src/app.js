64aed17a8d4d806e23e7fb88c6035c4c5c22b5f5
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-10-15 11:31:44 +0500

    changes in dependieces in backend app

--- BlogApp/Backend/src/app.js
+++ BlogApp/Backend/src/app.js
@@ -5,8 +5,12 @@ import { errorHandler } from "./utils/errorHandler.js";
 import cookieParser from "cookie-parser";
 const app = express();
 // Dependencies
-app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(cookieParser());
 // Routes
 app.use("/api/v1", authRouter);
