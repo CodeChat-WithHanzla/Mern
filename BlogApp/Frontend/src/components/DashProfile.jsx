a93512544aa084eda5615167cc9aa536809ef5c8
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-10-18 16:11:09 +0500

    remove unnessary things

--- BlogApp/Frontend/src/components/DashProfile.jsx
+++ BlogApp/Frontend/src/components/DashProfile.jsx
@@ -1,18 +1,16 @@
 import { useSelector } from 'react-redux'
 import { Alert, Button, TextInput } from 'flowbite-react'
 import { useRef, useState } from 'react'
-import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 function DashProfile() {
     const { currentUser } = useSelector(state => state.user)
     const [imgUrl, setImgUrl] = useState(null)
-    const [uploadProgress, SetImgProgress] = useState(null)
     const [uploadError, SetUploadError] = useState(null)
     const [formData, setFormData] = useState({})
-
-
     const filePickerRef = useRef()
     const handleChange = (e) => {
+        if (uploadError)
+            SetUploadError(null)
         const { id, value, files } = e.target
 
         if (files && files[0]) {
@@ -31,6 +29,7 @@ function DashProfile() {
             setFormData((prevData) => ({ ...prevData, [id]: typeof value === 'string' ? value.trim() : value, }))
     }
     const handleSubmit = async (e) => {
+        SetUploadError(null)
         e.preventDefault()
         const payload = new FormData()
         Object.entries(formData).forEach(([key, value]) => {
@@ -42,10 +41,10 @@ function DashProfile() {
                 body: payload
             });
             const data = await response.json()
-            if (!data.success) return uploadError("An error occurred while uploading. Please try again")
+            if (data.success === false) return SetUploadError("An error occurred while uploading. Please try again")
 
         } catch (error) {
-            SetUploadError('Updating error :: ', error)
+            SetUploadError(`Updating error :: ${error}`)
         }
     }
 
@@ -55,8 +54,7 @@ function DashProfile() {
             <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
                 <input type="file" accept='image/*' onChange={handleChange} ref={filePickerRef} hidden />
                 <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full relative" onClick={() => filePickerRef.current.click()}>
-                    {uploadProgress && (<CircularProgressbar value={uploadProgress || 0} text={`${uploadProgress}%`} strokeWidth={5} styles={{ root: { width: '100%', height: '100%', position: 'absolute' }, path: { stroke: `rgba(62,152,199,${uploadProgress / 100})` } }} />)}
-                    <img src={imgUrl || currentUser?.data?.ProfilePicture} alt="user" className={`rounded-full w-full h-full border-8 border-[lightgray] object-cover ${uploadProgress && uploadProgress < 100 && 'opacity-60'}`} />
+                    <img src={imgUrl || currentUser?.data?.ProfilePicture} alt="user" className={`rounded-full w-full h-full border-8 border-[lightgray] object-cover `} />
                 </div>
                 {
                     uploadError && (
