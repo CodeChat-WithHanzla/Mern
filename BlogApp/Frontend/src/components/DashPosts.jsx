96bcbd24fb92a8de2715e5a4d0a38554427236c8
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-10-28 14:57:51 +0500

    Add show more functionality in Dashboard posts section

--- BlogApp/Frontend/src/components/DashPosts.jsx
+++ BlogApp/Frontend/src/components/DashPosts.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from "react-redux";
-import { Table } from 'flowbite-react'
+import { Button, Table } from 'flowbite-react'
 import { Link } from 'react-router-dom'
 function DashPosts() {
     const { currentUser } = useSelector(state => state.user)
     const [userPost, setUserPost] = useState([])
+    const [showMore, setShowMore] = useState(true)
     console.log(userPost);
 
     const fetchPosts = async () => {
         try {
             const res = await fetch(`/api/v1/posts/get-posts?userId=${currentUser._id}`)
             const data = await res.json()
-            if (res.ok)
+            if (res.ok) {
                 setUserPost(data.posts)
+                if (data.posts.length < 9)
+                    setShowMore(false)
+            }
             console.log(data);
         } catch (error) {
             console.log(error.message);
@@ -23,6 +27,23 @@ function DashPosts() {
         if (currentUser.isAdmin)
             fetchPosts()
     }, [currentUser._id])
+    const handleShowMore = async () => {
+        const startIndex = userPost.length
+        try {
+            const res = await fetch(`/api/v1/posts/get-posts?userId=${currentUser._id}&startIndex=${startIndex}`)
+            const data = await res.json()
+            console.log(data);
+
+            if (res.ok) {
+                setUserPost((prev) => [...prev, ...data.posts])
+                if (data.posts.length < 9)
+                    setShowMore(false)
+            }
+        } catch (error) {
+            console.log(error.message);
+
+        }
+    }
     return (
         <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>{currentUser.isAdmin && userPost.length > 0 ? (
             <>
@@ -38,7 +59,7 @@ function DashPosts() {
                         </Table.HeadCell>
                     </Table.Head>
                     {userPost.map((post) =>
-                    (<Table.Body className='divide-y'>
+                    (<Table.Body className='divide-y' key={post._id}>
                         <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                             <Table.Cell>
                                 {new Date(post?.updatedAt).toLocaleDateString()}
@@ -66,6 +87,13 @@ function DashPosts() {
                     </Table.Body>)
                     )}
                 </Table>
+                {
+                    showMore && (
+                        <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                            Show More
+                        </button>
+                    )
+                }
             </>
         ) : <p>You have no posts yet</p>}</div>
     )
