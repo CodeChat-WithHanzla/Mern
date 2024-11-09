8274f1b031112675127adc4c4139584ab76a4b0c
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-11-09 18:16:30 +0500

    I Think the project is ready for deploy!

--- BlogApp/Backend/src/app.js
+++ BlogApp/Backend/src/app.js
@@ -8,6 +8,8 @@ import {
 import cors from "cors";
 import { errorHandler } from "./utils/errorHandler.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+const _dirname = path.resolve();
 const app = express();
 // Dependencies
 app.use(
@@ -24,5 +26,9 @@ app.use("/api/v1", authRouter);
 app.use("/api/v1", userRouter);
 app.use("/api/v1", postRouter);
 app.use("/api/v1", commentRouter);
+app.use(express.static(path.join(_dirname, "/Frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "Frontend", "dist", "index.html"));
+});
 app.use(errorHandler);
 export default app;
