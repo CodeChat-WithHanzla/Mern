1b2f81db7dbe09cb3ec7eb1b03a67221eb1af136
CodeChat-WithHanzla <hanzlaasam@gmail.com>
2024-11-16 01:07:03 +0500

    Create the React app using Vite and set up a configuration file for Firebase. Next, I will start building the homepage.

---
+++ AmazoneClone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-et2TiPFPvkMf9tpkHT_j--pUURm6Ep8",
+  authDomain: "challenge-681e3.firebaseapp.com",
+  projectId: "challenge-681e3",
+  storageBucket: "challenge-681e3.firebasestorage.app",
+  messagingSenderId: "678018137931",
+  appId: "1:678018137931:web:60be07fef950f1d53272ce",
+  measurementId: "G-59344J341T",
+};
